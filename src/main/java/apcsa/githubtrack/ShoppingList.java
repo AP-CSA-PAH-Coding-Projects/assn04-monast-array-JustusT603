18e47282321288de70865aa77fcb3f11d2a6bb7f
Justus Tam <justustam23@gmail.com>
2026-01-26 10:16:40 -0500

    Skeleton draft for ShoppingList class

--- src/main/java/apcsa/githubtrack/ShoppingList.java
+++ src/main/java/apcsa/githubtrack/ShoppingList.java
@@ -1,8 +1,21 @@
 package apcsa.githubtrack;
 
 // Implement the ShoppingList class here
+/* This class represents a shopping list, uses a custom implementation of ArrayList, 
+and stores ShoppingList objects. */ 
 
 public class ShoppingList implements MyList 
 {
-    
+    // Create instance variables
+    private int capacity;
+    private int size;
+    private ShoppingItem[] storage;
+
+    // Constructor
+    public ShoppingList()
+    {
+        this.capacity = 8;
+        this.size = 0;
+        this.ShoppingItem = new ShoppingItem[8];
+    }
 }
